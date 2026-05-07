55e38e3dadc8f5527bbb60c70d7f1523e49558ab
Biel <biel.ballara@asatorras.cat>
2026-05-07 09:52:43 +0200

    ej6,7,chall1

---
+++ portfolio-programacion-bielballara/python_Biel/challeng1.py
@@ -0,0 +1,17 @@
+# Demanem a l'usuari la base del rectangle
+base = float(input("Introdueix la base del rectangle: "))
+
+# Demanem a l'usuari l'altura del rectangle
+altura = float(input("Introdueix l'altura del rectangle: "))
+
+# Calculem l'àrea del rectangle
+area = base * altura
+
+# Calculem el perímetre del rectangle
+perimetre = 2 * (base + altura)
+
+# Mostrem l'àrea per pantalla
+print("L'àrea del rectangle és:", area)
+
+# Mostrem el perímetre per pantalla
+print("El perímetre del rectangle és:", perimetre)
---
+++ portfolio-programacion-bielballara/python_Biel/ejercicio6.py
@@ -0,0 +1,5 @@
+edat = int(input("Quants anys tens? "))
+
+espelmes = edat + 1
+
+print("Has de comprar", espelmes, "espelmes")
